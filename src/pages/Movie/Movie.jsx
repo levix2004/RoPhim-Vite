509f55fdc46e3447cab9538d693b6c1b8b82d9e1
levix2004 <levanvi220044@gmail.com>
2025-08-03 23:48:56 +0700

    doing Movie...

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -2,6 +2,8 @@ import { useParams } from 'react-router-dom';
 import styles from './Movie.module.scss';
 import classNames from 'classnames/bind';
 import { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faComment, faHeart, faPlay, faPlus, faShare } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
@@ -47,7 +49,7 @@ function Movie() {
                             <p>IMDb {movie?.tmdb.vote_average}</p>
                             <p>{movie?.year}</p>
                             <p>{movie?.time}</p>
-                            
+
                             <p>{movie?.status}</p>
                         </div>
                         <div className={cx('movie-categories')}>
@@ -56,24 +58,53 @@ function Movie() {
                             ))}
                         </div>
                         <div className={cx('movie-desc')}>
-                            <p className={cx("label")}>Giới thiệu: </p>
+                            <p className={cx('label')}>Giới thiệu: </p>
                             <p>{movie.content}</p>
                         </div>
                         <div className={cx('movie-time')}>
-                            <p className={cx("label")}>Thời lượng: </p>
+                            <p className={cx('label')}>Thời lượng: </p>
                             <p>{movie.time}</p>
                         </div>
                         <div className={cx('movie-coun')}>
-                            <p className={cx("label")}>Quốc gia: </p>
+                            <p className={cx('label')}>Quốc gia: </p>
                             <p>{movie.country[0].name}</p>
                         </div>
                         <div className={cx('movie-direc')}>
-                            <p className={cx("label")}>Đạo diễn</p>
+                            <p className={cx('label')}>Đạo diễn</p>
                             <p>{movie.director[0]}</p>
                         </div>
                     </div>
                 </div>
-                <div className={cx('movie-main')}></div>
+                <div className={cx('movie-main')}>
+                    <div className={cx('inner')}>
+                        <div className={cx('toolbar')}>
+                            <div className={cx('watch-btn')}>
+                                <FontAwesomeIcon icon={faPlay} />
+                                <p>Xem ngay</p>
+                            </div>
+                            <div className={cx('like-btn')}>
+                                <FontAwesomeIcon icon={faHeart} className={cx('icon')}/>
+                                <p>Yêu thích</p>
+                            </div>
+                            <div className={cx('add-btn')}>
+                                <FontAwesomeIcon icon={faPlus} className={cx('icon')}/>
+                                <p>Thêm vào</p>
+                            </div>
+                            <div className={cx('share-btn')}>
+                                <FontAwesomeIcon icon={faShare} className={cx('icon')}/>
+                                <p>Chia sẻ</p>
+                            </div>
+                            <div className={cx('cmt-btn')}>
+                                <FontAwesomeIcon icon={faComment} className={cx('icon')}/>
+                                <p>Bình luận</p>
+                            </div>
+                        </div>
+                        <div className={cx("content-box")}>
+                            <div className={cx('tabs-nav')}></div>
+                            <div className={cx('tab-content')}></div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
